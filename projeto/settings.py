a0ab41617668d5d623a52d3eec97cadee3d899fb
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-11-03 11:37:24 -0300

    melhorias  e inserções no djnago

--- projeto/settings.py
+++ projeto/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            BASE_DIR / 'base_templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   BASE_DIR / 'base_static',
+]
+# STATIC_ROOT = BASE_DIR / 'static',
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
