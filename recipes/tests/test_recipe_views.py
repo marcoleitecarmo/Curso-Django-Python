783d0958e055bdcaf1c76dfd15612ab785ffc4a3
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-12-15 18:23:18 -0300

    erros nos testes a serem corrigidos

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,14 +1,9 @@
 from django.urls import resolve, reverse
 from recipes import views
-# from unittest import skip
 from .test_recipe_base import RecipeTestBase
 
 
-# @skip('WIP')
 class RecipeViewsTest(RecipeTestBase):
-    def tearDown(self) -> None:
-        return super().tearDown()
-
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -31,24 +26,18 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_home_template_loads_recipes(self):
         # Need a recipe for this test
         self.make_recipe()
-
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
-
         # Check if one recipe exists
         self.assertIn('Recipe Title', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_home_template_dont_load_recipes_not_published(self):
         """Test recipe is_published False dont show"""
         # Need a recipe for this test
         self.make_recipe(is_published=False)
-
         response = self.client.get(reverse('recipes:home'))
-
         # Check if one recipe exists
         self.assertIn(
             '<h1>No recipes found here 🥲</h1>',
@@ -67,6 +56,24 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
@@ -81,10 +88,8 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipe_detail_template_loads_the_correct_recipe(self):
         needed_title = 'This is a detail page - It load one recipe'
-
         # Need a recipe for this test
         self.make_recipe(title=needed_title)
-
         response = self.client.get(
             reverse(
                 'recipes:recipe',
@@ -94,6 +99,28 @@ class RecipeViewsTest(RecipeTestBase):
             )
         )
         content = response.content.decode('utf-8')
-
         # Check if one recipe exists
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id': recipe.id
+                }
+            )
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_uses_correct_view_function(self):
+        resolved = resolve(reverse('recipes:search'))
+        self.assertIs(resolved.func, views.search)
+
+    def test_recipe_search_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,9 @@ from .import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('recipes/category/<int:category_id>/',
-         views.category, name='category'),
-    path('recipes/<int:id>/', views.recipe, name="recipe"),
-    path('recipes/search/', lambda request:..., name="search"),
+        path('', views.home, name="home"),
+        path('recipes/search/', views.search, name="search"),
+        path('recipes/category/<int:category_id>/',
+             views.category, name="category"),
+        path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
